f64f234c79e14ee969810e88b2072f6e17e0f2c6
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-08-13 15:16:27 -0700

    creates Projects component

---
+++ src/components/Projects.js
@@ -0,0 +1,11 @@
+import React, {Componet} from 'react';
+
+function Projects () {
+    return (
+        <div>
+            <p>project links + descriptions here</p>
+        </div>
+    )
+}
+
+export default Projects

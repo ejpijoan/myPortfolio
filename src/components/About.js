9fd003112d5556fe2297182d0b94daa55e193418
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-08-13 15:13:10 -0700

    creates About component

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react' 
+
+function About () {
+    retrun (
+        <div>
+            <h3>All about me!</h3>
+            <p>My background, skills, and interests</p>
+        </div>
+    )
+}
+
+export default About

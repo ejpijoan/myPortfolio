a093b90fdcfdaf6f57e4ccc97c54c2569dc14868
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-08-13 17:25:40 -0700

    imorts About compnent to Profile container

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
-import Intro from '../components/Intro'
+import Intro from '../components/Intro';
+import About from '../components/About'
 
 class Profile extends Component {
     render() {
         return (
-            <Intro/>
+            <div>
+                <Intro/>
+                <About/>
+            </div>
         )
     }
 }

fad003d199c7489e3f9b0083f3c04815a67ec89c
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-08-13 17:21:55 -0700

    creates Socials component

---
+++ src/components/Socials.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react' 
+
+function Socials () {
+    return (
+        <div>
+            <p>social media links</p>
+        </div>
+    )
+}
+
+export default Socials
+

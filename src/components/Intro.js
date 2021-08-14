0ab707ccd856fabb5f6cb3683c1b01c162b7ab6a
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-08-13 19:59:54 -0700

    gives Intro component local state

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,7 +1,14 @@
 import React, {Component} from 'react';
 
 class Intro extends Component {
-        
+        constructor() {
+                super();
+                this.state = {
+                roles: ['full stack software engineer', 'biochemist', 'calligrapher', 'sewist']
+                }
+        }
+       
+
         render(){
            return <h3>Hi, My name is Emiko. I'm a full stack software engineer.</h3>     
         }      

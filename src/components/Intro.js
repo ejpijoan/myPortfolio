90f8e08188b52172c83d0f3be0a948aeb9e81f1f
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-08-14 14:03:12 -0700

    defines changeRoles function in Intro component

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -5,9 +5,12 @@ class Intro extends Component {
                 super();
                 this.state = {
                 roles: ['full stack software engineer', 'biochemist', 'calligrapher', 'sewist']
-                }
+                };
         }
-       
+
+        changeRoles = () => {
+             setInterval(this.currentRole(), 1500)           
+        }
 
         render(){
            return <h3>Hi, My name is Emiko. I'm a full stack software engineer.</h3>     

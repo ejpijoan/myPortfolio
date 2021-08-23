09347b1bdcfa06f9eac3d22c39517e6fd300e0d9
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-08-22 20:27:29 -0700

    adds title, github link and description for Recipie Box in Projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,7 +4,8 @@ function Projects () {
     return (
         <div>
             <h3>Projects</h3>
-            <p>project 1</p>
+            <h4>Recipe Box</h4><a href='https://github.com/Emi691/recipe-box'>GitHub</a>
+            <p>Recipe box is a SPA application that allows users to save, add, and create recipes online.</p>
             <p>project 2</p>
             <p>project 3</p>
         </div>

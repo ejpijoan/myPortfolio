7905302bcbfbf322f8e117b4bb341401cd13525c
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-08-22 20:28:30 -0700

    adds Gihub link to socials component

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -6,6 +6,8 @@ function Socials () {
             <a href='https://www.linkedin.com/in/emiko-nagasawa-pijoan/'>LinkedIn</a>
             <br/>
             <a href='https://www.facebook.com/emiko.nagasawapijoan'>Facebook</a>
+            <br/>
+            <a href='https://github.com/Emi691'>Github</a>
         </div>
     )
 }

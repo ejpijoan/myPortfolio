4d16324d42126a5872b244eff2d55576e1a15322
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-08-22 20:16:12 -0700

    adds about blurb to about component

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,8 @@ function About () {
     return (
         <div>
             <h3>All about me!</h3>
-            <p>My background, skills, and interests</p>
+            <p>Full stack software engineer with a strong background in chemistry. I am proficient in Ruby and JavaScript (ES6) and have experience with Rails and React frameworks. I am also a graduate of the University of California at Santa Cruz, where I majored in chemistry with a biochemistry concentration.
+            I am excited to be starting a new career path as a software developer. I am eager to learn new languages and skills and continue to grow as a programmer and a scientist. Using my background in biochemistry and my passion for creating innovative new applications I hope to be able to contribute to exciting advancements in a dynamic field.</p>
         </div>
     )
 }

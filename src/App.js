aa24a314568daa4c04603228d9d94bb751140dbf
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-09-12 14:04:20 -0700

    imports fontawesome core and necessary icons and adds thme to library

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import './App.css';
 import Profile from './containers/Profile';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faMail } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faMail)
 
 function App() {
   return (

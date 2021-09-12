b4ad5e5a8269714941260b4767cf849b8d3d0af0
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-09-12 14:09:52 -0700

    installs free regular icons for fontawesome

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,8 +1,10 @@
-import React from 'react' 
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' 
 
 function Socials () {
     return (
         <div>
+            <FontAwesomeIcon icon={['fab', 'linkedin']} />
             <a href='https://www.linkedin.com/in/emiko-nagasawa-pijoan/'>LinkedIn</a>
             <br/>
             <a href='https://www.facebook.com/emiko.nagasawapijoan'>Facebook</a>

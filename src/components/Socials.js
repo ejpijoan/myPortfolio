ec3e2ef4de21dd525f0647e022cfc82b0d53e05f
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-09-12 14:14:59 -0700

    displays icons for social media in socials component

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -4,11 +4,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function Socials () {
     return (
         <div>
-            <FontAwesomeIcon icon={['fab', 'linkedin']} />
+            <FontAwesomeIcon icon={['fab', 'linkedin-in']} />
             <a href='https://www.linkedin.com/in/emiko-nagasawa-pijoan/'>LinkedIn</a>
             <br/>
+            <FontAwesomeIcon icon={['fab', 'facebook-f']} />
             <a href='https://www.facebook.com/emiko.nagasawapijoan'>Facebook</a>
             <br/>
+            <FontAwesomeIcon icon={['fab', 'github']} />
             <a href='https://github.com/Emi691'>Github</a>
         </div>
     )

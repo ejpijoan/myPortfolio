a226002edcbd7b107af0776d5784a2ae3a992e03
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-10-22 16:34:45 -0700

    imports components for routing and changes dic to react partial in app.js

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Intro from './components/Intro'
+import Profile from './containers/Profile'
 
 library.add(fab, faEnvelope)
 
 function App() {
   return (
     <Router>
-      <div>
+      <>
       <Route exact path='/' component={Intro} />
       <Route exact path='/profile' component={Profile} />
-      </div>
+      </>
     </Router>
   )
 }

c20b80c59550c6690ec97c0f6d5d1973f87e470b
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-10-22 16:30:19 -0700

    adds routing in app.js for intro and profile pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import './App.css';
-import Profile from './containers/Profile';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 library.add(fab, faEnvelope)
 
 function App() {
   return (
-    <Profile/>
+    <Router>
+      <div>
+      <Route exact path='/' component={Intro} />
+      <Route exact path='/profile' component={Profile} />
+      </div>
+    </Router>
   )
 }
 

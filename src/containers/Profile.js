20d6c5f5d9e88f51896601a10327c0fd901b4f72
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-10-24 20:12:52 -0700

    removes state and event listeners from profile container component, and simply renders About, Projects, Socials, and Resume

--- src/containers/Profile.js
+++ src/containers/Profile.js
@@ -1,36 +1,18 @@
 import React, {Component} from 'react';
-import Intro from '../components/Intro';
 import About from '../components/About';
 import Projects from '../components/Projects';
 import Resume from '../components/Resume';
 import Socials from '../components/Socials';
 
 class Profile extends Component {
-    constructor(){
-        super();
-        this.state = {
-            components: [<Intro/>]
-        }
-    }
-
-    componentDidMount = () => {
-        document.addEventListener('click', this.handleClick)
-    }
-
-    componentDidUpdate = () => {
-        document.removeEventListener('click', this.handleClick)
-    }
-
-    handleClick = () => {
-        this.setState( prevState => ({
-            components: [...prevState.components, <About/>, <Projects/>, <Resume/>, <Socials/>]
-        }))
-    }
 
     render() {
         return (
             <div>
-                {this.state.components}
+                <About/>
+                <Projects/>
+                <Resume/>
+                <Socials/>
             </div>
         )
     }

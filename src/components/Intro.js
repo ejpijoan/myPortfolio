9e5da987467ca48da1c57ca320951bac92bbe5e4
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-10-31 16:09:17 -0700

    removes onlclick event redirect from intro component

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -12,16 +12,11 @@ class Intro extends Component {
         }
 
         componentDidMount = () => {
-                document.addEventListener('click', this.handleClick)
                 setTimeout(this.currentRole,1500)
         }
 
         componentDidUpdate = () => {
                 setTimeout(this.currentRole,1500)
-        }
-
-        componentWillUnmount = () => {
-                document.removeEventListener('click', this.handleClick)
         }
 
         currentRole = () => {
@@ -30,17 +25,7 @@ class Intro extends Component {
               }))
         }
 
-        handleClick = () => {
-                this.setState({
-                        redirect: true
-                })
-        }
-
         render(){
-           if(this.state.redirect){
-                return <Redirect push to='/profile' />
-           } 
-
            return (
                 <h1>
                 Hi, My name is Emiko. I'm a {this.state.roles[this.state.index]}

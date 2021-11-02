18df8c3e5d03f0d1a78a5dd0e853ea599fd8038c
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-11-02 14:30:57 -0700

    formats brand header in navbar to be bold

--- src/Navigation.js
+++ src/Navigation.js
@@ -5,12 +5,12 @@ import {Navbar, Nav, Container} from 'react-bootstrap'
 class Navigation extends Component {
     render(){
         return(
-            <Navbar className='Navbar' bg='white'>
+            <Navbar className='navbar' bg='white'>
                 <Container className='justify-content-end'>
                     <Nav className='flex-column'>
-                            <Navbar.Brand href="/">Emiko</Navbar.Brand>
-                            <Nav.Link href="/about">About</Nav.Link>
-                            <Nav.Link href="/portfolio">Work</Nav.Link>
+                            <Navbar.Brand className='brand' href="/">Emiko</Navbar.Brand>
+                            <Nav.Link className='navlink' href="/about">About</Nav.Link>
+                            <Nav.Link className='navlink' href="/portfolio">Work</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>    

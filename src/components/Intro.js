1e4b954ca4e27a5c77e3f60ea6fef0ef74bfccb3
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-11-03 19:55:09 -0700

    changes container className to container and removes temporary background color styling

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -27,7 +27,7 @@ class Intro extends Component {
  
         render(){
            return (
-                <Container className='align-items-center'>
+                <Container className='container'>
                         <h1 className='intro' contentEditable>
                         Hi, My name is Emiko. I'm a <span style={{color: 'tomato'}}>{this.state.roles[this.state.index]}</span></h1>
                 </Container>

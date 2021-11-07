cbe6aef7dd1217eccdec58e5e6b522f178f7fd26
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-11-07 15:18:56 -0800

    cleans up formating for render in intro component

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -29,7 +29,9 @@ class Intro extends Component {
            return (
                 <Container className='container'>
                         <h1 className='intro'>
-                        Hi, My name is Emiko. I'm a <span style={{color: 'tomato'}}>{this.state.roles[this.state.index]}</span></h1>
+                        Hi, My name is Emiko. I'm a 
+                        <span style={{color: 'tomato'}}>{this.state.roles[this.state.index]}</span>
+                        </h1>
                 </Container>
            )
         }    

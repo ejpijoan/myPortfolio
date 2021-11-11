644157db469576a64d6bf4e312be9457203f32b9
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-11-11 15:09:07 -0800

    changes social component from a function to a class

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,18 +1,21 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' 
 
-function Socials () {
-    return (
-        <div>
-            <a href='https://www.linkedin.com/in/emiko-nagasawa-pijoan/'><FontAwesomeIcon icon={['fab', 'linkedin-in']} /></a>
-            <br/>
-            <a href='https://www.facebook.com/emiko.nagasawapijoan'><FontAwesomeIcon icon={['fab', 'facebook-f']} /></a>
-            <br/>
-            <a href='https://github.com/Emi691'><FontAwesomeIcon icon={['fab', 'github']} /></a>
-            <br/>
-            <button className='email' ><FontAwesomeIcon icon={['far', 'envelope']} /></button>
-        </div>
-    )
+class Socials extends Component() {
+    
+    render(){
+        return (
+            <div>
+                <a href='https://www.linkedin.com/in/emiko-nagasawa-pijoan/'><FontAwesomeIcon icon={['fab', 'linkedin-in']} /></a>
+                <br/>
+                <a href='https://www.facebook.com/emiko.nagasawapijoan'><FontAwesomeIcon icon={['fab', 'facebook-f']} /></a>
+                <br/>
+                <a href='https://github.com/Emi691'><FontAwesomeIcon icon={['fab', 'github']} /></a>
+                <br/>
+                <button className='email' onClick={this.clickHandler}><FontAwesomeIcon icon={['far', 'envelope']} /></button>
+            </div>
+        )
+    }
 }
 
 export default Socials

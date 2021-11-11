b607cbadcf0e5060f2814fd4860f5a34b35117bb
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-11-11 15:53:50 -0800

    imports css file to about component and resizes image

--- src/components/About.js
+++ src/components/About.js
@@ -1,3 +1,4 @@
+import '../css/About.css'
 import React from 'react' 
 import Profile from '../IMG_2043.JPG'
 
@@ -7,7 +8,7 @@ function About () {
             <h3>All about me!</h3>
             <p>Full stack software engineer with a strong background in chemistry. I am proficient in Ruby and JavaScript (ES6) and have experience with Rails and React frameworks. I am also a graduate of the University of California at Santa Cruz, where I majored in chemistry with a biochemistry concentration.
             I am excited to be starting a new career path as a software developer. I am eager to learn new languages and skills and continue to grow as a programmer and a scientist. Using my background in biochemistry and my passion for creating innovative new applications I hope to be able to contribute to exciting advancements in a dynamic field.</p>
-            <img src={Profile}></img>
+            <img className='profile' src={Profile} width='200'></img>
         </div>
     )
 }

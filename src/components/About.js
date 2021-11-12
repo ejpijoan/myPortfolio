dc2ad034bb05114eeb94d17d38a283b8f81fcf7f
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-11-11 16:14:36 -0800

    adds alt to profile image

--- src/components/About.js
+++ src/components/About.js
@@ -2,13 +2,14 @@ import '../css/About.css'
 import React from 'react' 
 import Profile from '../attachments/profile.JPG'
 
+
 function About () {
     return (
         <div>
             <h3>All about me!</h3>
             <p>Full stack software engineer with a strong background in chemistry. I am proficient in Ruby and JavaScript (ES6) and have experience with Rails and React frameworks. I am also a graduate of the University of California at Santa Cruz, where I majored in chemistry with a biochemistry concentration.
             I am excited to be starting a new career path as a software developer. I am eager to learn new languages and skills and continue to grow as a programmer and a scientist. Using my background in biochemistry and my passion for creating innovative new applications I hope to be able to contribute to exciting advancements in a dynamic field.</p>
-            <img className='profile' src={Profile} width='200'></img>
+            <img className='profile' src={Profile} width='200' alt=''></img>
         </div>
     )
 }

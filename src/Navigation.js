42eca70f31557d046719c94eba3402a8d43738b5
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-11-18 22:15:22 -0800

    changes navbar to be offcanvas with a hamburger

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,18 +1,30 @@
 import './Navigation.css';
 import React, {Component} from 'react'
-import {Navbar, Nav, Container, NavDropdown} from 'react-bootstrap'
+import {Navbar, Nav, Container, Offcanvas} from 'react-bootstrap'
 
 class Navigation extends Component {
     render(){
         return(
-            <Navbar className='navbar' bg='white'>
-                <Container className='justify-content-end'>
-                    <NavDropdown className='dropdown' title='dropdown'>
-                        <NavDropdown.Item className='navlink' href="/">Emiko</NavDropdown.Item>
-                        <NavDropdown.Divider/>
-                        <NavDropdown.Item className='navlink' href="/about">About</NavDropdown.Item>
-                        <NavDropdown.Item className='navlink' href="/portfolio">Work</NavDropdown.Item>
-                    </NavDropdown>
+            <Navbar className='navbar' bg='light' expand={false}>
+                <Container fluid>
+                    <Navbar.Brand className='brand' href="/">Emiko</Navbar.Brand>
+                    <Navbar.Toggle aria-controls='offcanvasNavbar' />
+                    <Navbar.Offcanvas 
+                    id='offcanvasNavbar' 
+                    aria-labelledby='offcanvasNavbarLabel'
+                    placement='end'
+                    >
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id='NavbarLabel'>Emiko</Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <Nav className='justify-content-end flex-grow-1 pe-3'>
+                                <Nav.Link className='navlink' href="/about">About</Nav.Link>
+                                <Nav.Link className='navlink' href="/portfolio">Work</Nav.Link>
+                            </Nav>
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                        
                 </Container>
             </Navbar>    
         )

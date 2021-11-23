2fb6335e8d096053d31b30892e4983ae48059dab
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-11-23 15:52:10 -0800

    adds class names and encases rendered parts in a react-bootstrap container for about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,6 @@
 import React from 'react' 
 import '../css/About.css'
+import {Container} from 'react-bootstrap'
 import Socials from '../components/Socials';
 import Profile from '../attachments/profile.JPG'
 
@@ -7,11 +8,13 @@ import Profile from '../attachments/profile.JPG'
 function About () {
     return (
         <div>
-            <h3>All about me!</h3>
-            <p>Full stack software engineer with a strong background in chemistry. I am proficient in Ruby and JavaScript (ES6) and have experience with Rails and React frameworks. I am also a graduate of the University of California at Santa Cruz, where I majored in chemistry with a biochemistry concentration.
-            I am excited to be starting a new career path as a software developer. I am eager to learn new languages and skills and continue to grow as a programmer and a scientist. Using my background in biochemistry and my passion for creating innovative new applications I hope to be able to contribute to exciting advancements in a dynamic field.</p>
-            <img className='profile' src={Profile} width='200' alt=''></img>
-            <Socials/>
+            <Container className='about'>
+                <h3 className='header'>All about me!</h3>
+                <p className='story'>Full stack software engineer with a strong background in chemistry. I am proficient in Ruby and JavaScript (ES6) and have experience with Rails and React frameworks. I am also a graduate of the University of California at Santa Cruz, where I majored in chemistry with a biochemistry concentration.
+                I am excited to be starting a new career path as a software developer. I am eager to learn new languages and skills and continue to grow as a programmer and a scientist. Using my background in biochemistry and my passion for creating innovative new applications I hope to be able to contribute to exciting advancements in a dynamic field.</p>
+                <img className='profile' src={Profile} width='200' alt=''></img>
+                <Socials/>
+            </Container>
         </div>
     )
 }

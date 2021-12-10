101d8f268134e6d9f32af187d3e086cebdf1c1ab
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-12-10 12:33:36 -0800

    changes app.js to a react fragment instead of a div and adds a switch component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navigation from '../src/Navigation'
 import Intro from './components/Intro'
 import About from './components/About'
@@ -12,16 +12,16 @@ library.add(fab, faEnvelope)
 
 function App() {
   return (
-    <div className='App'>
+    <>
       <Navigation/>
       <Router>
-        <>
-        <Route exact path='/' component={Intro} />
-        <Route exact path='/about' component={About} />
-        <Route exact path='/portfolio' component={Projects} />
-        </>
+        <Switch>
+          <Route exact path='/' component={Intro} />
+          <Route exact path='/about' component={About} />
+          <Route exact path='/portfolio' component={Projects} />
+        </Switch>
       </Router>
-    </div>
+    </>
   )
 }
 
